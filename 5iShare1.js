47858a67bbed09615402a12e25b6535953d39369
yiyiBeauty <2661380190@qq.com>
2014-10-24 23:21:21 +0800

    fsdf

---
+++ 5iShare1.js
@@ -0,0 +1,10 @@
+﻿
+var contentConfig = {
+    'baseTimes': 15205231,
+    'shareSpeed': 10,
+    'logoUrl':'https://raw.githubusercontent.com/yiyiBeauty/5iShare/master/old.jpg',
+    'sharedDescContent':'1：不要奢望别人给你经济上的任何帮助，钱对任何人都是不够用的 （学会给与）',
+    'sharedTitle':'一位老人的九句话，疯传了sharedTimes次',
+    'sharedTimeLineTitle':'一位老人的九句话，疯传了sharedTimes次',
+    'sharedCoverUp':'这份感动已经被分享了sharedTimes次。',
+};
