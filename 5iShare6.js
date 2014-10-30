08dd1a4602faf4149716f09abed5207a31260d7e
yiyiBeauty <695698637@qq.com>
2014-10-30 15:39:43 +0800

    法撒旦法
    fasfs

--- 5iShare6.js
+++ 5iShare6.js
@@ -2,7 +2,7 @@ var contentConfig = {
     'htmlTitle':'小盆友牵着哪只小鸭子？',
     'baseTimes': 421,
     'shareSpeed': 0.03,
-    'logoUrl':'http://img.blog.csdn.net/20141030133323406http://yspe2371e4aa7697989.yunshipei.cn/dHlwZT1mdyZzaXplPTI0MCZzcmM9YUhSMGNDVXpRU1V5UmlVeVJtbHRaeTVpYkc5bkxtTnpaRzR1Ym1WMEpUSkdNakF4TkRFd016QXhOVE13TURjeU9EUT0=',
+    'logoUrl':'http://img.blog.csdn.net/20141030153007284',
     'sharedDescContent':'90%都说看得头很痛！',
     'sharedTitle':'小盆友牵着哪只小鸭子？，已经有sharedTimes个人找到答案了。',
     'sharedTimeLineTitle':'小盆友牵着哪只小鸭子？，已经有sharedTimes个人找到答案了。',
