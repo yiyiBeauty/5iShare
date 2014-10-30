b5d60c3118e78af56a50757ec9ef331d3995d752
yiyiBeauty <695698637@qq.com>
2014-10-30 15:35:44 +0800

    fasdfa
    fsdfa

---
+++ 5iShare6.js
@@ -0,0 +1,20 @@
+var contentConfig = {
+    'htmlTitle':'小盆友牵着哪只小鸭子？',
+    'baseTimes': 421,
+    'shareSpeed': 0.03,
+    'logoUrl':'http://img.blog.csdn.net/20141030133323406http://yspe2371e4aa7697989.yunshipei.cn/dHlwZT1mdyZzaXplPTI0MCZzcmM9YUhSMGNDVXpRU1V5UmlVeVJtbHRaeTVpYkc5bkxtTnpaRzR1Ym1WMEpUSkdNakF4TkRFd016QXhOVE13TURjeU9EUT0=',
+    'sharedDescContent':'90%都说看得头很痛！',
+    'sharedTitle':'小盆友牵着哪只小鸭子？，已经有sharedTimes个人找到答案了。',
+    'sharedTimeLineTitle':'小盆友牵着哪只小鸭子？，已经有sharedTimes个人找到答案了。',
+    'sharedCoverUp':'想看答案，分享后知晓。已经有sharedTimes个人分享了。',
+};
+
+var fullContent = "小盆友牵着哪只小鸭子？\n\n    哈哈，你猜对了，就是第一只！";
+
+var partContent = "小盆友牵着哪只小鸭子？";
+
+
+
+
+
+dataInited = true;
