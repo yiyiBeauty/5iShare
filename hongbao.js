3270bebfc030812673d4dcc3dd6f2df632685c0f
yiyiBeauty <695698637@qq.com>
2014-12-23 23:21:19 +0800

    红包

--- hongbao.js
+++ hongbao.js
@@ -1,3 +1,5 @@
 var hongbaoUrls = [
   /*1 yuan*/"https://wxapp.tenpay.com/v2/hybrid/www/weixin/hongbao/hb_detail.shtml?showwxpaytitle=1&sendid=1000000000201412232035603319&pagetype=2&detailToke=gIoYfgy3Q8KntmWA4o_NOSA726QlqIlgE-XE7RkFQg0oiKC_ds6JkNEHCfWrsQngJEjJz7zt7TY="
+  /*2 yuan*/,"https://wxapp.tenpay.com/v2/hybrid/www/weixin/hongbao/receive.shtml?showwxpaytitle=1&sendid=1000000000201412232035662417&channelid=1&msgtype=1&from=singlemessage&isappinstalled=0"
+  /*20yuan*/,"https://wxapp.tenpay.com/v2/hybrid/www/weixin/hongbao/hb_detail.shtml?showwxpaytitle=1&pagetype=2&sendid=1000000000201412232035665495&detailToke=dmr2fF9tZZuZ052fS4WUkHlcWT4ZcYByrVlwm-eoMe1GEE22Vo2zIfAXZPQlze14J8UCKZuF76I%3D&from_busi="
 ];
