5dd0167788bab1c3c8e5daec27e0923ab6337902
yiyiBeauty <695698637@qq.com>
2014-12-23 21:42:40 +0800

    红包

---
+++ hongbao.js
@@ -0,0 +1,3 @@
+var hongbaoUrls = [
+  /*1 yuan*/"https://wxapp.tenpay.com/v2/hybrid/www/weixin/hongbao/hb_detail.shtml?showwxpaytitle=1&sendid=1000000000201412232035603319&pagetype=2&detailToke=gIoYfgy3Q8KntmWA4o_NOSA726QlqIlgE-XE7RkFQg0oiKC_ds6JkNEHCfWrsQngJEjJz7zt7TY="
+];
